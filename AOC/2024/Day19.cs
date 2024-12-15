66aa7fb2b7d9bd43981aa4e83171fab3eb6be9e1
Aceless34 <nico.hundertmark@gmx.de>
2024-12-15 01:06:39 +0100

    Added days

---
+++ AOC/2024/Day19.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC._2024
+{
+    public class Day19 : Day
+    {
+        public string Task1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Task2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AOC/Program.cs
+++ AOC/Program.cs
@@ -24,6 +24,16 @@ namespace AOC
                 {12, new Day12() },
                 {13, new Day13() },
                 {14, new Day14() },
+                {15, new Day15() },
+                {16, new Day16() },
+                {17, new Day17() },
+                {18, new Day18() },
+                {19, new Day19() },
+                {20, new Day20() },
+                {21, new Day21() },
+                {22, new Day22() },
+                {23, new Day23() },
+                {24, new Day24() }
             };
 
             //2024
