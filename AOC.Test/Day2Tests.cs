222d2351b00508d8cf407a234b3239614af65df9
Aceless34 <nico.hundertmark@gmx.de>
2025-12-02 13:43:02 +0100

    Solved Day 2

---
+++ AOC.Test/Day2Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace AOC.Test
+{
+    public class Day2Tests : SolverTestBase<AOC._2025.Day2>
+    {
+        protected override string SampleInput => @"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+  
+        [Fact]
+        public void Part1_SolveExample()
+        {
+            var result = Solver.SolvePart1(SampleInput);
+            result.Should().Be(1227775554);
+        }
+
+        [Fact]
+        public void Part2_SolveExample()
+        {
+            var result = Solver.SolvePart2(SampleInput);
+            result.Should().Be(4174379265);
+        }
+
+    }
+}
---
+++ AOC/2025/Day2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC._2025
+{
+    public class Day2 : ISolver
+    {
+        public uint Day => 2;
+        public uint Year => 2025;
+
+        public string Title => "Gift Shop";
+
+        public object SolvePart1(string input)
+        {
+            IEnumerable<(string start, string end)> ranges= input.Split(",").Select(range => (range.Split("-")[0], range.Split("-")[1]));
+
+            long sum = 0;
+            foreach((string start, string end) range in ranges)
+            {
+                for(long i = long.Parse(range.start); i <= long.Parse(range.end); i++)
+                {
+                    string numString = i.ToString();
+                    if(numString.Length % 2 == 0)
+                    {
+                        string left = numString.Substring(0, numString.Length / 2);
+                        string right = numString.Substring(numString.Length / 2);
+                        if (left == right)
+                        {
+                            sum += i;
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public object SolvePart2(string input)
+        {
+            IEnumerable<(string start, string end)> ranges = input.Split(",").Select(range => (range.Split("-")[0], range.Split("-")[1]));
+
+            long sum = 0;
+            foreach ((string start, string end) range in ranges)
+            {
+                for (long i = long.Parse(range.start); i <= long.Parse(range.end); i++)
+                {
+                    string numString = i.ToString();
+                    if(Regex.Match(numString, @"^(.+?)\1+$").Success)
+                    {
+                        sum += i;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
