6435cc891945e698181b83ae3d7f8a69a7df6730
Aceless34 <nico.hundertmark@gmx.de>
2025-12-05 11:54:54 +0100

    solved day 5

---
+++ AOC.Test/Day5Tests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace AOC.Test
+{
+    public class Day5Tests : SolverTestBase<AOC._2025.Day5>
+    {
+        protected override string SampleInput => @"3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+  
+        [Fact]
+        public void Part1_SolveExample()
+        {
+            var result = Solver.SolvePart1(SampleInput);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Part2_SolveExample()
+        {
+            var result = Solver.SolvePart2(SampleInput);
+            result.Should().Be(14);
+        }
+
+    }
+}
---
+++ AOC/2025/Day5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC._2025
+{
+    public class Day5 : ISolver
+    {
+        public uint Day => 5;
+
+        public uint Year => 2025;
+
+        public string Title => "Cafeteria";
+
+        public object SolvePart1(string input)
+        {
+            string rangesString = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)[0];
+            string ingredientIdsString = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)[1];
+
+            List<(long min, long max)> ranges = rangesString.Split(Environment.NewLine).Select(rs => (long.Parse(rs.Split("-")[0]), long.Parse(rs.Split("-")[1]))).ToList();
+            List<long> ingredientIds = ingredientIdsString.Split("\n").Select(id => long.Parse(id)).ToList();
+
+            return ingredientIds.Where(id => ranges.Any(r => id >= r.min && id <= r.max)).Count();
+
+        }
+
+        public object SolvePart2(string input)
+        {
+            string rangesString = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)[0];
+            List<(long min, long max)> ranges = rangesString.Split(Environment.NewLine).Select(rs => (min: long.Parse(rs.Split("-")[0]), max: long.Parse(rs.Split("-")[1]))).OrderBy(r => r.min).ToList();
+
+            var merged = new List<(long min, long max)>();
+
+            foreach (var r in ranges)
+            {
+                if (merged.Count == 0 || r.min > merged[^1].max + 1)
+                    merged.Add(r);
+                else
+                    merged[^1] = (merged[^1].min, Math.Max(merged[^1].max, r.max));
+            }
+
+            long total = merged.Sum(r => r.max + 1 - r.min);
+            return total;
+        }
+    }
+}
